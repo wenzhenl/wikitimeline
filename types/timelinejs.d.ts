668921e6350f79852664358cf2aabf0a1d41d1f6
App Builder <buildsomethingpeoplewant.gmail.com>
2024-07-02 23:17:30 -0700

    fix build issue

---
+++ types/timelinejs.d.ts
@@ -0,0 +1,6 @@
+declare module '@knight-lab/timelinejs' {
+    // Add specific types for Timeline if available, otherwise use 'any'
+    export class Timeline {
+      constructor(element: HTMLElement, data: any, options?: any);
+    }
+  }
