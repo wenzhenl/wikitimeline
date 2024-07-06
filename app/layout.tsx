2911dc46261ed3e9e9132c9614484931516dd4b9
App Builder <buildsomethingpeoplewant.gmail.com>
2024-07-05 23:10:04 -0700

    wikitimeline

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "WIKITIMELINE",
+  title: "WikiTimeline",
   description: "Generate Timeline From Wikipedia",
 };
 
