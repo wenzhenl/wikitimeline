7669944eb7d57bf5df6aa27a57c7917d0a9aafb5
App Builder <buildsomethingpeoplewant.gmail.com>
2024-07-07 17:49:50 -0700

    integrate with wikipedia and openai

--- app/timeline/[pageName]/page.tsx
+++ app/timeline/[pageName]/page.tsx
@@ -2,7 +2,10 @@ import { PrismaClient } from "@prisma/client";
 import { GetServerSidePropsContext } from "next";
 import dynamic from "next/dynamic";
 import { Event } from "@/app/components/MyTimelineComponent";
+import wiki from "wikipedia";
+import OpenAI from "openai";
 
+const openai = new OpenAI();
 const prisma = new PrismaClient();
 const DynamicTimeline = dynamic(
   () => import("@/app/components/MyTimelineComponent"),
@@ -17,10 +20,57 @@ interface SearchParams {
   url: string;
 }
 
-const getTimeline = async (pageName: string) => {
-  return await prisma.timeline.findUnique({
-    where: { pageName_language: { pageName: pageName, language: "en" } },
+const extractWikiPage = async (pageName: string) => {
+  try {
+    const wikiPage = await wiki.content(pageName);
+    console.log(wikiPage);
+    return wikiPage;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const summarizeWiki2Timeline = async (wikiPage: string) => {
+  const completion = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "system",
+        content: "",
+      },
+      { role: "user", content: wikiPage },
+    ],
+    model: "gpt-4o",
+    response_format: { type: "json_object" },
   });
+  console.log(completion.choices[0].message.content);
+
+  return completion.choices[0].message.content;
+};
+
+const getTimeline = async (pageName: string) => {
+  const language = "en";
+  const timeline = await prisma.timeline.findUnique({
+    where: { pageName_language: { pageName: pageName, language: language } },
+  });
+
+  if (!timeline) {
+    const wikiPage = await extractWikiPage(pageName);
+    if (!wikiPage) return timeline;
+    const summary = await summarizeWiki2Timeline(wikiPage);
+    if (!summary) return timeline;
+    const timelineData = JSON.parse(summary);
+
+    const newTimeline = await prisma.timeline.create({
+      data: {
+        pageName,
+        language,
+        wikipediaPage: `https://${language}.wikipedia.org/wiki/${pageName}`,
+        timelineData: timelineData,
+      },
+    });
+    return newTimeline;
+  }
+  return timeline;
 };
 
 const isEventArray = (data: any): data is Event[] => {
