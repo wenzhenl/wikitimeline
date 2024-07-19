343792e415360566fb1727be116fdb808a6e658f
App Builder <buildsomethingpeoplewant.gmail.com>
2024-07-19 07:08:40 -0700

    add thumb url

--- app/api/wiki/route.ts
+++ app/api/wiki/route.ts
@@ -1,6 +1,11 @@
 import { NextResponse } from 'next/server';
 import wiki from 'wikipedia';
 
+type WikiResponse = {
+  wikiPage: string;
+  thumbnailSource?: string;
+};
+
 export async function GET(request: Request) {
   const { searchParams } = new URL(request.url);
   const pageName = searchParams.get('pageName');
@@ -11,7 +16,15 @@ export async function GET(request: Request) {
 
   try {
     const wikiPage = await wiki.content(pageName);
-    return NextResponse.json({ wikiPage });
+    const wikiSummary = await wiki.summary(pageName);
+
+    const response: WikiResponse = { wikiPage };
+
+    if (wikiSummary.thumbnail && wikiSummary.thumbnail.source) {
+      response.thumbnailSource = wikiSummary.thumbnail.source;
+    }
+
+    return NextResponse.json(response);
   } catch (error: unknown) {
     if (error instanceof Error) {
       return NextResponse.json({ error: error.message }, { status: 500 });
