6d8fece03d3376b6732cf894aa42ed8fd4f12bcd
App Builder <buildsomethingpeoplewant.gmail.com>
2024-07-20 00:36:23 -0700

    add google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -17,8 +18,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-      <GoogleAnalytics gaId="G-WPG6VTRDW9" />
+      <body>
+        {children}
+        <GoogleAnalytics gaId="G-WPG6VTRDW9" />
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8046438874699734"
+          crossorigin="anonymous"
+          strategy="afterInteractive"
+        />
+      </body>
     </html>
   );
 }
