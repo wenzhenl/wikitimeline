009a94918800edc45ac8d2a36d9f5e66965b0797
App Builder <buildsomethingpeoplewant.gmail.com>
2024-07-20 16:07:09 -0700

    bring modal to front

--- app/components/SaveButton.tsx
+++ app/components/SaveButton.tsx
@@ -40,8 +40,8 @@ const SaveButton: React.FC<SaveButtonProps> = ({ pageNames }) => {
             Save
           </button>
           {showModal && (
-            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center">
-              <div className="bg-white p-4 rounded">
+            <div className="fixed inset-0 bg-gray-600 bg-opacity-50 flex justify-center items-center z-50">
+              <div className="bg-white p-4 rounded z-50">
                 <h2 className="text-2xl mb-4">Save Collection</h2>
                 <input
                   type="text"
