44d07a426da4d5e34a2b85fe04b5d0d299ea627f
wenzhenl <liyukuang@gmail.com>
2024-12-07 00:05:46 -0800

    make timeline page prettier

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,6 +14,25 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      keyframes: {
+        blob: {
+          "0%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+          "33%": {
+            transform: "translate(30px, -50px) scale(1.1)",
+          },
+          "66%": {
+            transform: "translate(-20px, 20px) scale(0.9)",
+          },
+          "100%": {
+            transform: "translate(0px, 0px) scale(1)",
+          },
+        },
+      },
+      animation: {
+        blob: "blob 7s infinite",
+      },
     },
   },
   plugins: [],
