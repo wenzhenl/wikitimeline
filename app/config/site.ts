868c4824a73394047df2c19c6ce1ababb6635ccf
wenzhenl <liyukuang@gmail.com>
2024-12-10 23:05:41 -0800

    f

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,4 +1,4 @@
 export const SITE_CONFIG = {
   DOMAIN: process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000',
-  URLS_PER_SITEMAP: 40000,
+  URLS_PER_SITEMAP: 5, // change back to 40000 when launched
 } as const 
