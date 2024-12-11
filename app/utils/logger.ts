6645d0a7b91ea17cf5952086ab60c6b1959e786c
wenzhenl <liyukuang@gmail.com>
2024-12-10 20:32:30 -0800

    only log in non prod

---
+++ app/utils/logger.ts
@@ -0,0 +1,25 @@
+type LogLevel = 'debug' | 'info' | 'warn' | 'error';
+
+const logger = {
+  debug: (...args: any[]) => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.debug('[DEBUG]', ...args);
+    }
+  },
+  
+  info: (...args: any[]) => {
+    if (process.env.NODE_ENV !== 'production') {
+      console.log('[INFO]', ...args);
+    }
+  },
+  
+  warn: (...args: any[]) => {
+    console.warn('[WARN]', ...args);
+  },
+  
+  error: (...args: any[]) => {
+    console.error('[ERROR]', ...args);
+  }
+};
+
+export default logger; 
