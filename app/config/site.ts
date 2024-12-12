9be2489adcc13f1618e419ed62b70c76edbbe924
wenzhenl <liyukuang@gmail.com>
2024-12-11 21:17:06 -0800

    change url 40000

--- app/config/site.ts
+++ app/config/site.ts
@@ -2,5 +2,5 @@ export const SITE_CONFIG = {
   DOMAIN: process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     : 'http://localhost:3000',
-  URLS_PER_SITEMAP: 5,
+  URLS_PER_SITEMAP: 40000,
 } as const 
