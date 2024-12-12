a8cc4269b94462817fc771c06dc5201b3310d146
wenzhenl <liyukuang@gmail.com>
2024-12-11 23:37:46 -0800

    fix math issue

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -8,7 +8,7 @@ export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   try {
     const keys = await redis.keys('timeline:*')
     const totalKeys = keys.length
-    const sitemapCount = Math.ceil(totalKeys / SITE_CONFIG.URLS_PER_SITEMAP * 2.0)
+    const sitemapCount = Math.ceil(totalKeys * 2.0 / SITE_CONFIG.URLS_PER_SITEMAP)
 
     const staticSitemap = {
       url: `${SITE_CONFIG.DOMAIN}/sitemaps/static.xml`,
