837f6b72adb6058f21c1f881f76880737fa8cf4a
wenzhenl <liyukuang@gmail.com>
2024-12-14 22:11:16 -0800

    client side env var need prefix next_public_

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,6 +1,6 @@
 function validateEnvVars() {
-  if (!process.env.GOOGLE_ANALYTICS_ID) {
-    throw new Error('GOOGLE_ANALYTICS_ID environment variable is not set');
+  if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID) {
+    throw new Error('NEXT_PUBLIC_GOOGLE_ANALYTICS_ID environment variable is not set');
   }
 }
 
@@ -12,5 +12,5 @@ export const SITE_CONFIG = {
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     : 'http://localhost:3000',
   URLS_PER_SITEMAP: 40000,
-  GOOGLE_ANALYTICS_ID: process.env.GOOGLE_ANALYTICS_ID,
-} as const 
+  GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+} as const
