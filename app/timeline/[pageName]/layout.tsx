064b79667ba51eb1e097566b36f3d438c6cccccf
wenzhenl <liyukuang@gmail.com>
2024-12-14 23:16:11 -0800

    add support for oembed

--- app/timeline/[pageName]/layout.tsx
+++ app/timeline/[pageName]/layout.tsx
@@ -1,5 +1,6 @@
 import { Redis } from "@upstash/redis";
 import logger from "@/app/utils/logger";
+import { SITE_CONFIG } from "@/app/config/site";
 
 // Initialize Redis
 const redis = Redis.fromEnv();
@@ -44,6 +45,9 @@ export async function generateMetadata({
     .map((summary) => summary.thumbnail)
     .filter((url): url is string => !!url);
 
+  const url = `${SITE_CONFIG.DOMAIN}/timeline/${params.pageName}`;
+  const embedUrl = `${SITE_CONFIG.DOMAIN}/timeline/${params.pageName}/embed`;
+
   return {
     title: `Timeline of ${title}`,
     description: `Historical timeline of events related to ${title}, generated from Wikipedia content.`,
@@ -52,12 +56,38 @@ export async function generateMetadata({
       description: `Historical timeline of events related to ${title}, generated from Wikipedia content.`,
       images: images.length > 0 ? images : undefined,
       type: "article",
+      url,
     },
     twitter: {
       card: "summary_large_image",
       title: `Timeline of ${title}`,
       description: `Historical timeline of events related to ${title}, generated from Wikipedia content.`,
       images: images.length > 0 ? [images[0]] : undefined,
+    },
+    alternates: {
+      types: {
+        // oEmbed endpoint
+        'application/json+oembed': `${SITE_CONFIG.DOMAIN}/api/oembed?url=${encodeURIComponent(url)}`,
+      },
+    },
+    other: {
+      // Reddit embed specific meta tags
+      'reddit:embed:url': embedUrl,
+      'reddit:embed:width': '800',
+      'reddit:embed:height': '600',
+      // oEmbed discovery tags
+      'oembed:type': 'rich',
+      'oembed:url': url,
+      'oembed:provider_name': 'WikiTimeline',
+      'oembed:provider_url': SITE_CONFIG.DOMAIN,
+      'oembed:title': `Timeline of ${title}`,
+      'oembed:description': `Historical timeline of events related to ${title}, generated from Wikipedia content.`,
+      'oembed:thumbnail_url': images[0],
+      'oembed:thumbnail_width': '1200',
+      'oembed:thumbnail_height': '630',
+      'oembed:width': '800',
+      'oembed:height': '600',
+      'oembed:html': `<iframe src="${embedUrl}" width="800" height="600" frameborder="0" allowfullscreen></iframe>`,
     },
   };
 }
