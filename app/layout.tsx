2cf0af75b1ae8cb69269d617512c728e30979d76
wenzhenl <liyukuang@gmail.com>
2025-01-12 18:51:46 -0800

    wtf so strict

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import { GoogleAnalytics } from "@next/third-parties/google";
-import Script from "next/script";
 import "./globals.css";
 import { SITE_CONFIG } from "./config/site";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "WikiTimeline",
