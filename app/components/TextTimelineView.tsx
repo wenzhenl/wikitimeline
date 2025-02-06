2b4c579e3b051d2235fa1811823767486629ac2e
wenzhenl <liyukuang@gmail.com>
2025-02-06 00:47:57 -0800

    add end of timeline

--- app/components/TextTimelineView.tsx
+++ app/components/TextTimelineView.tsx
@@ -120,6 +120,20 @@ export default function TextTimelineView({ data }: TextTimelineViewProps) {
           </p>
         </div>
       )}
+
+      <div
+        className="
+        mt-8 
+        pt-6 
+        border-t 
+        border-gray-200 dark:border-gray-700
+        text-center 
+        text-gray-500 dark:text-gray-400 
+        text-sm
+      "
+      >
+        ● End of Timeline ●
+      </div>
     </div>
   );
 }
