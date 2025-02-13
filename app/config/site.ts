8a4059bb9d6eb9c20f0e43188cfe4d5d0b88741c
wenzhenl <liyukuang@gmail.com>
2025-02-12 18:33:07 -0800

    make google analytics id optional

--- app/config/site.ts
+++ app/config/site.ts
@@ -1,12 +1,3 @@
-function validateEnvVars() {
-  if (!process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID) {
-    throw new Error('NEXT_PUBLIC_GOOGLE_ANALYTICS_ID environment variable is not set');
-  }
-}
-
-// Run validation immediately
-validateEnvVars();
-
 export const SITE_CONFIG = {
   DOMAIN: process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL}`
@@ -14,6 +5,6 @@ export const SITE_CONFIG = {
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     : 'http://localhost:3000',
   URLS_PER_SITEMAP: 40000,
-  GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID,
+  GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '',
   ADSENSE_CLIENT_ID: process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID || '',
 } as const
