a1dc0d1ca329e5180f23faca775f4dbb956e3fd5
wenzhenl <liyukuang@gmail.com>
2025-02-27 16:50:18 -0800

    remove unused variables

--- app/config/site.ts
+++ app/config/site.ts
@@ -4,7 +4,6 @@ export const SITE_CONFIG = {
     : process.env.VERCEL_PROJECT_PRODUCTION_URL
     ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
     : 'http://localhost:3000',
-  URLS_PER_SITEMAP: 40000,
   GOOGLE_ANALYTICS_ID: process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID || '',
   ADSENSE_CLIENT_ID: process.env.NEXT_PUBLIC_ADSENSE_CLIENT_ID || '',
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '',
