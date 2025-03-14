fac0fb43cfad6416c624137f6cd65695786e6325
wenzhenl <liyukuang@gmail.com>
2025-03-14 16:07:08 -0700

    decreast event for now

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,5 +1,5 @@
 export const PAGE_DELIMITER = "|";
-export const MIN_NUM_EVENTS_FOR_TIMELINE = 10;
+export const MIN_NUM_EVENTS_FOR_TIMELINE = 1;
 
 // Page name constants
 export const PAGE_NAME_SEPARATOR = ":::";
