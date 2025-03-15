159e5ba284724f6f0c929ddb630d307cb177a22f
wenzhenl <liyukuang@gmail.com>
2025-03-14 17:45:38 -0700

    decrease temperature

--- app/constants/gemini/index.ts
+++ app/constants/gemini/index.ts
@@ -1,5 +1,5 @@
 // General Gemini settings
-export const TEMPERATURE = 0.5;
+export const TEMPERATURE = 0.2;
 export const TOP_P = 0.9;
 export const TOP_K = 40;
 export const PRESENCE_PENALTY = 0.3;
